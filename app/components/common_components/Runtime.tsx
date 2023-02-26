c0212d1b2c17a05da4bf5a64fef73c54d4aaae17
av-erencelik <erenmemo05@gmail.com>
2023-02-26 15:05:55 +0300

    fixed runtime display bug

--- app/components/common_components/Runtime.tsx
+++ app/components/common_components/Runtime.tsx
@@ -1,10 +1,14 @@
 import { Text } from "@chakra-ui/react";
 
-const Runtime = ({ runtime }: { runtime: number[] }) => {
+const Runtime = ({ runtime }: { runtime: number }) => {
   return (
-    <Text>
-      {Math.floor(runtime[0] / 60)}h {runtime[0] % 60}m
-    </Text>
+    <>
+      {runtime && (
+        <Text>
+          {Math.floor(runtime / 60)}h {runtime % 60}m
+        </Text>
+      )}
+    </>
   );
 };
 
